b96dc2c673842badf82df00c9c56186fec6b12ce
Yash Kadve <yashkadve4@gmail.com>
2023-12-13 16:23:04 +0530

    Configure SQL Server

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -2,6 +2,7 @@ using eTickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,9 +24,9 @@ namespace eTickets
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
             //DBContext Configuration
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));  //Added nuget SQLServer package 5.0.1 v
 
             services.AddControllersWithViews();
         }
