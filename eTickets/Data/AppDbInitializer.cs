6720775ab65f41e460156d097d58e639a193cf8f
Yash Kadve <yashkadve4@gmail.com>
2023-12-25 15:42:38 +0530

    Change method frm savechanges to executerawprocedure

--- eTickets/Data/AppDbInitializer.cs
+++ eTickets/Data/AppDbInitializer.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Data
 {
@@ -23,354 +24,38 @@ namespace eTickets.Data
                 //Cinema
                 if (!context.Cinemas.Any())
                 {
-                    try
-                    {
-                        context.Cinemas.AddRange(new List<Cinema>()
-                    {
-                        new Cinema()
-                        {
-                            Name = "Cinema 1",
-                            Logo = "http://dotnethow.net/images/cinemas/cinema-1.jpeg",
-                            Description = "This is the description of the first cinema"
-                        },
-                        new Cinema()
-                        {
-                            Name = "Cinema 2",
-                            Logo = "http://dotnethow.net/images/cinemas/cinema-2.jpeg",
-                            Description = "This is the description of the first cinema"
-                        },
-                        new Cinema()
-                        {
-                            Name = "Cinema 3",
-                            Logo = "http://dotnethow.net/images/cinemas/cinema-3.jpeg",
-                            Description = "This is the description of the first cinema"
-                        },
-                        new Cinema()
-                        {
-                            Name = "Cinema 4",
-                            Logo = "http://dotnethow.net/images/cinemas/cinema-4.jpeg",
-                            Description = "This is the description of the first cinema"
-                        },
-                        new Cinema()
-                        {
-                            Name = "Cinema 5",
-                            Logo = "http://dotnethow.net/images/cinemas/cinema-5.jpeg",
-                            Description = "This is the description of the first cinema"
-                        },
-                    });
-                        context.SaveChanges();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Log or output the inner exception details
-                        Console.WriteLine($"Exception Type: {ex.GetType().FullName}");
-                        Console.WriteLine($"Exception Message: {ex.Message}");
-                        Console.WriteLine($"Inner Exception: {ex.InnerException?.Message}");
-                        Console.WriteLine($"Stack Trace: {ex.StackTrace}");
-                    }
+                    context.Database.ExecuteSqlRaw("INSERT INTO Cinemas (Name, Logo, Description) VALUES ('Cinema 1', 'http://dotnethow.net/images/cinemas/cinema-1.jpeg', 'This is the description of the first cinema')");
+                    context.Database.ExecuteSqlRaw("INSERT INTO Cinemas (Name, Logo, Description) VALUES ('Cinema 2', 'http://dotnethow.net/images/cinemas/cinema-2.jpeg', 'This is the description of the second cinema')");
+                    // Add more cinemas as needed
+
                 }
                 //Actors
                 if (!context.Actors.Any())
                 {
-                    try
-                    {
-                        context.Actors.AddRange(new List<Actor>()
-                    {
-                        new Actor()
-                        {
-                            FullName = "Actor 1",
-                            Bio = "This is the Bio of the first actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-1.jpeg"
-
-                        },
-                        new Actor()
-                        {
-                            FullName = "Actor 2",
-                            Bio = "This is the Bio of the second actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-2.jpeg"
-                        },
-                        new Actor()
-                        {
-                            FullName = "Actor 3",
-                            Bio = "This is the Bio of the second actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-3.jpeg"
-                        },
-                        new Actor()
-                        {
-                            FullName = "Actor 4",
-                            Bio = "This is the Bio of the second actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-4.jpeg"
-                        },
-                        new Actor()
-                        {
-                            FullName = "Actor 5",
-                            Bio = "This is the Bio of the second actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-5.jpeg"
-                        }
-                    });
-                        context.SaveChanges();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Log or output the inner exception details
-                        Console.WriteLine($"Exception Type: {ex.GetType().FullName}");
-                        Console.WriteLine($"Exception Message: {ex.Message}");
-                        Console.WriteLine($"Inner Exception: {ex.InnerException?.Message}");
-                        Console.WriteLine($"Stack Trace: {ex.StackTrace}");
-                    }
+                    context.Database.ExecuteSqlRaw("INSERT INTO Actors (FullName, Bio, ProfilePictureURL) VALUES ('Actor 1', 'This is the Bio of the first actor', 'http://dotnethow.net/images/actors/actor-1.jpeg')");
+                    context.Database.ExecuteSqlRaw("INSERT INTO Actors (FullName, Bio, ProfilePictureURL) VALUES ('Actor 2', 'This is the Bio of the second actor', 'http://dotnethow.net/images/actors/actor-2.jpeg')");
+                    // Add more actors as needed
                 }
                 //Producers
                 if (!context.Producers.Any())
                 {
-                    try
-                    {
-                        context.Producers.AddRange(new List<Producer>()
-                    {
-                        new Producer()
-                        {
-                            FullName = "Producer 1",
-                            Bio = "This is the Bio of the first actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/producers/producer-1.jpeg"
-
-                        },
-                        new Producer()
-                        {
-                            FullName = "Producer 2",
-                            Bio = "This is the Bio of the second actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/producers/producer-2.jpeg"
-                        },
-                        new Producer()
-                        {
-                            FullName = "Producer 3",
-                            Bio = "This is the Bio of the second actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/producers/producer-3.jpeg"
-                        },
-                        new Producer()
-                        {
-                            FullName = "Producer 4",
-                            Bio = "This is the Bio of the second actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/producers/producer-4.jpeg"
-                        },
-                        new Producer()
-                        {
-                            FullName = "Producer 5",
-                            Bio = "This is the Bio of the second actor",
-                            ProfilePictureURL = "http://dotnethow.net/images/producers/producer-5.jpeg"
-                        }
-                    });
-                        context.SaveChanges();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Log or output the inner exception details
-                        Console.WriteLine($"Exception Type: {ex.GetType().FullName}");
-                        Console.WriteLine($"Exception Message: {ex.Message}");
-                        Console.WriteLine($"Inner Exception: {ex.InnerException?.Message}");
-                        Console.WriteLine($"Stack Trace: {ex.StackTrace}");
-                    }
+                    context.Database.ExecuteSqlRaw("INSERT INTO Producers (FullName, Bio, ProfilePictureURL) VALUES ('Producer 1', 'This is the Bio of the first producer', 'http://dotnethow.net/images/producers/producer-1.jpeg')");
+                    context.Database.ExecuteSqlRaw("INSERT INTO Producers (FullName, Bio, ProfilePictureURL) VALUES ('Producer 2', 'This is the Bio of the second producer', 'http://dotnethow.net/images/producers/producer-2.jpeg')");
+                    // Add more producers as needed
                 }
                 //Movies
                 if (!context.Movies.Any())
                 {
-                    try
-                    {
-                        context.Movies.AddRange(new List<Movie>()
-                    {
-                        new Movie()
-                        {
-                            Name = "Life",
-                            Description = "This is the Life movie description",
-                            Price = 39.50,
-                            ImageURL = "http://dotnethow.net/images/movies/movie-3.jpeg",
-                            StartDate = DateTime.Now.AddDays(-10),
-                            EndDate = DateTime.Now.AddDays(10),
-                            CinemaId = 3,
-                            ProducerId = 3,
-                            MovieCategory = MovieCategory.Documentary
-                        },
-                        new Movie()
-                        {
-                            Name = "The Shawshank Redemption",
-                            Description = "This is the Shawshank Redemption description",
-                            Price = 29.50,
-                            ImageURL = "http://dotnethow.net/images/movies/movie-1.jpeg",
-                            StartDate = DateTime.Now,
-                            EndDate = DateTime.Now.AddDays(3),
-                            CinemaId = 1,
-                            ProducerId = 1,
-                            MovieCategory = MovieCategory.Action
-                        },
-                        new Movie()
-                        {
-                            Name = "Ghost",
-                            Description = "This is the Ghost movie description",
-                            Price = 39.50,
-                            ImageURL = "http://dotnethow.net/images/movies/movie-4.jpeg",
-                            StartDate = DateTime.Now,
-                            EndDate = DateTime.Now.AddDays(7),
-                            CinemaId = 4,
-                            ProducerId = 4,
-                            MovieCategory = MovieCategory.Horror
-                        },
-                        new Movie()
-                        {
-                            Name = "Race",
-                            Description = "This is the Race movie description",
-                            Price = 39.50,
-                            ImageURL = "http://dotnethow.net/images/movies/movie-6.jpeg",
-                            StartDate = DateTime.Now.AddDays(-10),
-                            EndDate = DateTime.Now.AddDays(-5),
-                            CinemaId = 1,
-                            ProducerId = 2,
-                            MovieCategory = MovieCategory.Documentary
-                        },
-                        new Movie()
-                        {
-                            Name = "Scoob",
-                            Description = "This is the Scoob movie description",
-                            Price = 39.50,
-                            ImageURL = "http://dotnethow.net/images/movies/movie-7.jpeg",
-                            StartDate = DateTime.Now.AddDays(-10),
-                            EndDate = DateTime.Now.AddDays(-2),
-                            CinemaId = 1,
-                            ProducerId = 3,
-                            MovieCategory = MovieCategory.Cartoon
-                        },
-                        new Movie()
-                        {
-                            Name = "Cold Soles",
-                            Description = "This is the Cold Soles movie description",
-                            Price = 39.50,
-                            ImageURL = "http://dotnethow.net/images/movies/movie-8.jpeg",
-                            StartDate = DateTime.Now.AddDays(3),
-                            EndDate = DateTime.Now.AddDays(20),
-                            CinemaId = 1,
-                            ProducerId = 5,
-                            MovieCategory = MovieCategory.Drama
-                        }
-                    });
-                        context.SaveChanges();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Log or output the inner exception details
-                        Console.WriteLine($"Exception Type: {ex.GetType().FullName}");
-                        Console.WriteLine($"Exception Message: {ex.Message}");
-                        Console.WriteLine($"Inner Exception: {ex.InnerException?.Message}");
-                        Console.WriteLine($"Stack Trace: {ex.StackTrace}");
-                    }
+                    context.Database.ExecuteSqlRaw("INSERT INTO Movies (Name, Description, Price, ImageURL, StartDate, EndDate, CinemaId, ProducerId, MovieCategory) VALUES ('Life', 'This is the Life movie description', 39.50, 'http://dotnethow.net/images/movies/movie-3.jpeg', GETDATE(), DATEADD(day, 10, GETDATE()), 2, 2, 'Documentary')");
+                    context.Database.ExecuteSqlRaw("INSERT INTO Movies (Name, Description, Price, ImageURL, StartDate, EndDate, CinemaId, ProducerId, MovieCategory) VALUES ('The Shawshank Redemption', 'This is the Shawshank Redemption description', 29.50, 'http://dotnethow.net/images/movies/movie-1.jpeg', GETDATE(), DATEADD(day, 3, GETDATE()), 1, 1, 'Action')");
+                    // Add more movies as needed
                 }
                 //Actors & Movies
                 if (!context.Actors_Movies.Any())
                 {
-                    try
-                    {
-                        context.Actors_Movies.AddRange(new List<Actor_Movie>()
-                    {
-                        new Actor_Movie()
-                        {
-                            ActorId = 1,
-                            MovieId = 1
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 3,
-                            MovieId = 1
-                        },
-
-                         new Actor_Movie()
-                        {
-                            ActorId = 1,
-                            MovieId = 2
-                        },
-                         new Actor_Movie()
-                        {
-                            ActorId = 4,
-                            MovieId = 2
-                        },
-
-                        new Actor_Movie()
-                        {
-                            ActorId = 1,
-                            MovieId = 3
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 2,
-                            MovieId = 3
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 5,
-                            MovieId = 3
-                        },
-
-
-                        new Actor_Movie()
-                        {
-                            ActorId = 2,
-                            MovieId = 4
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 3,
-                            MovieId = 4
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 4,
-                            MovieId = 4
-                        },
-
-
-                        new Actor_Movie()
-                        {
-                            ActorId = 2,
-                            MovieId = 5
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 3,
-                            MovieId = 5
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 4,
-                            MovieId = 5
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 5,
-                            MovieId = 5
-                        },
-
-
-                        new Actor_Movie()
-                        {
-                            ActorId = 3,
-                            MovieId = 6
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 4,
-                            MovieId = 6
-                        },
-                        new Actor_Movie()
-                        {
-                            ActorId = 5,
-                            MovieId = 6
-                        },
-                    });
-                        context.SaveChanges();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Log or output the inner exception details
-                        Console.WriteLine($"Exception Type: {ex.GetType().FullName}");
-                        Console.WriteLine($"Exception Message: {ex.Message}");
-                        Console.WriteLine($"Inner Exception: {ex.InnerException?.Message}");
-                        Console.WriteLine($"Stack Trace: {ex.StackTrace}");
-                    }
+                    context.Database.ExecuteSqlRaw("INSERT INTO Actors_Movies (ActorId, MovieId) VALUES (1,5)");
+                    context.Database.ExecuteSqlRaw("INSERT INTO Actors_Movies (ActorId, MovieId) VALUES (2, 6)");
+                    // Add more actor-movie relationships as needed
                 }
             }
         }
