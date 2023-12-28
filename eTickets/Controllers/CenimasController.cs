69b51f60d26dc16b4e8caf85a62b189d7a32e6c0
Yash Kadve <yashkadve4@gmail.com>
2023-12-28 11:47:16 +0530

    Added all Controllers

---
+++ eTickets/Controllers/CenimasController.cs
@@ -0,0 +1,24 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class CenimasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CenimasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
