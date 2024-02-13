50163618baad1e2cb59b0e267d9eb072b5db7cbd
Yash Kadve <yashkadve4@gmail.com>
2024-02-13 14:23:49 +0530

    Comit

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -21,7 +21,7 @@ namespace eTickets.Controllers
             _service = service;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index() 
         {
             var allMovies = await _service.GetAllAsync(n => n.Cinema);
             return View(allMovies);
