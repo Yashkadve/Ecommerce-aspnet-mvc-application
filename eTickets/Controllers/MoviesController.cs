41ac37b328ab2b1b5b915fafba26462bc417d32c
Yash Kadve <57342703+Yashkadve@users.noreply.github.com>
2024-02-23 10:17:33 +0530

    Comit checking

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace eTickets.Controllers
+namespace eTickets.Controllers 
 {
     public class MoviesController : Controller
     {
